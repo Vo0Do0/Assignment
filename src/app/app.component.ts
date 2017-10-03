0c0fce5a265750fce77b4c7d47333684601c1cda
Soumen Naskar <soumen.naskar@newrubric.com>
2017-10-03 11:07:52 +0530

    Commited changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,197 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public dataSet = [{
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}, {
+    "title": "A day at the potter's workshop in Bangalore",
+    "image": "https://s-media-cache-ak0.pinimg.com/originals/a2/f5/2b/a2f52b7e7845795ae8f8d2c06f1d4fda.jpg",
+    "quick_info": "Get a glimpse into the potter&#39;s life as you get down and get your hands dirty, feel the softness of soil and mould something beautiful!"
+}];
+  constructor() {
+
+  }
+
 }
